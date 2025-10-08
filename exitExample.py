966b6186756ccea391e1ef1e10bdb61e188b7de4
Kartik Chelagamsetty <104778557+ch-kartik@users.noreply.github.com>
2025-10-08 20:44:19 +0530

    Add files via upload

---
+++ exitExample.py
@@ -0,0 +1,11 @@
+
+# import sys module and use exit()
+
+import sys
+while True:
+    print('Type exit to exit')
+    response = input('>')
+    if response == 'exit':
+        sys.exit()
+    print('You typed ' + response + '.')
+
