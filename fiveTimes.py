a7bfef8c19e8ca8e2a421e285377d62b6572bf4a
Kartik Chelagamsetty <104778557+ch-kartik@users.noreply.github.com>
2025-10-09 22:18:49 +0530

    Add files via upload

---
+++ fiveTimes.py
@@ -0,0 +1,32 @@
+
+
+# for loop
+
+'''
+print('Hello!')
+for i in range(5):
+    print('On this iteration, i is set to ' + str(i))
+print('Goodbye!')
+
+'''
+
+'''
+# Sum of numbers 0 to 100
+total = 0
+for num in range(101):
+    total = total + num
+print(total)
+    
+'''
+
+# Equivalent while loop
+
+print('Hello')
+i = 0
+while i < 5:
+    print('On this iteration, i is set to ' + str(i))
+    i = i + 1
+print('Goodbye!')
+
+for i in range(5, -1, -1):
+    print(i)
