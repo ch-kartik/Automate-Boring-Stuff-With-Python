4e8592579c3a3ebf55b1934e1498276dbf3486ed
Kartik Chelagamsetty <104778557+ch-kartik@users.noreply.github.com>
2025-10-13 23:18:40 +0530

    Add files via upload

---
+++ localGlobalSameName.py
@@ -0,0 +1,12 @@
+def spam():
+    eggs = 'spam local'
+    print(eggs)  # Prints 'spam local'
+
+def bacon():
+    eggs = 'bacon local'
+    print(eggs)  # Prints 'bacon local'
+    spam()
+    print(eggs)  # Prints 'bacon local'
+
+eggs = 'global'
+bacon()
