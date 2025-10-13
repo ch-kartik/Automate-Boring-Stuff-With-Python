4825b2237ad771489484d603e3598a536d5c9a49
Kartik Chelagamsetty <104778557+ch-kartik@users.noreply.github.com>
2025-10-13 23:18:17 +0530

    Add files via upload

---
+++ littleKid.py
@@ -0,0 +1,14 @@
+
+# littleKid.py
+
+name = 'Carol'
+age = 3000
+if name == 'Alice':
+    print('Hi, Alice.')
+elif age < 12:
+    print('You are not Alice, kiddo.')
+else:
+    print('You are neither Alice nor a little kid.')
+
+
+
