6f3978712f6984dbc046798f4059defea47b51c5
Kartik Chelagamsetty <104778557+ch-kartik@users.noreply.github.com>
2025-10-14 22:04:10 +0530

    Add files via upload

---
+++ loops.py
@@ -0,0 +1,17 @@
+
+# Loops
+
+
+spam = 0
+if spam < 5:
+    print('Hello, world.')
+    spam = spam + 1
+
+
+
+spam = 0
+while spam < 5:
+    print('Hello, world. This is Chitti - The Robo.')
+    spam = spam + 1
+
+
