df6b663a356c1606736a1aff8a784cf436a67f80
Kartik Chelagamsetty <104778557+ch-kartik@users.noreply.github.com>
2025-10-23 23:50:42 +0530

    Add files via upload

---
+++ yourName.py
@@ -0,0 +1,37 @@
+
+
+"""
+name = ''
+while name != 'Chitti':
+    print('Please type your name.')
+    name = input('>')
+print('Thank you!')
+
+"""
+
+'''
+while True:
+    print('Please type your name.')
+    name = input('>')
+    if name == 'Chitti':
+        break
+print('Thank you!')
+
+'''
+
+
+
+# infinite loop
+while True:
+    print('Hello, world!')
+
+
+
+
+
+
+
+
+
+
+
